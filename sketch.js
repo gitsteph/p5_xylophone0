2d2cdbceebf98f77515ca0eba81ddbf44a979e5d
Stephanie H. Andrews <byronic.string@gmail.com>
2018-08-09 23:42:37 -0700

    Added sound and changed styling

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ var canvasHeight;
 var numTones;
 
 var allGeometries;
-const hoverDeviation = 100;
+const colorDeviation = 10;
+const hoverDeviation = 35;
 
 
 function setup() {
@@ -13,18 +14,33 @@ function setup() {
     var startPosX = 0;
     var startPosY = 100;
     var w = 100;
-    var h = 400;
+    var height = 400;
     for (var i=0; i < numTones; i++) {
+        // instantiate oscillator
+        var env = new p5.Envelope();
+        env.setADSR(.001, .2, .2, .5);
+        env.setRange(0.4, 0.0);
+        var osc = new p5.Oscillator();
+        osc.setType('sine');
+        var freqBase = 200 + i * (800 / numTones);
+        var freqDeviation = 20;
+        osc.freq(random((freqBase - freqDeviation), (freqBase + freqDeviation)));
+        osc.amp(env);
+        osc.start();
+
+        // declare rect properties
         var posX = startPosX + w * i;
         var posY = startPosY;
+        var rainbowHueBase = i * (255 / numTones);
         var obj = {
             'x': posX,
             'y': posY,
             'w': w,
-            'h': h,
-            'r': random(255),
-            'g': random(255),
-            'b': random(255),
+            'height': height,
+            'hue': int(random((rainbowHueBase - colorDeviation), (rainbowHueBase + colorDeviation))),
+            's': random(150, 255),
+            'b': random(150, 220),
+            'env': env,
             'mouseOverBool': false
         };
         allGeometries.push(obj);
@@ -33,35 +49,38 @@ function setup() {
     canvasWidth = windowWidth * 0.97;
     canvasHeight = windowHeight * 0.97;
     createCanvas(canvasWidth, canvasHeight);
-    background('#FAE');
+    background(random(['#CEE5F2', '#3A405A', '#FFD2FC', '#FFEBE7',]));
 }
 
 function mouseOverCheck(targetRect) {
     var isInsideTargetRect = false;
     if (mouseX >= targetRect.x && mouseX <= (targetRect.x + targetRect.w) 
-        && mouseY >= targetRect.y && mouseY <= (targetRect.y + targetRect.h)) {
+        && mouseY >= targetRect.y && mouseY <= (targetRect.y + targetRect.height)) {
         isInsideTargetRect = true;
     }
     return isInsideTargetRect;
 }
 
 function draw() {
-    cursor(HAND);
+    cursor(ARROW);
     for (var i = 0; i < allGeometries.length; i++) {
         var currentRect = allGeometries[i];
         currentRect.mouseOverBool = mouseOverCheck(currentRect);
         if (currentRect.mouseOverBool == true) {
-            var r = currentRect.r + hoverDeviation;
-            var g = currentRect.g + hoverDeviation;
+            cursor(HAND);
+            var hue = currentRect.hue;
+            var s = currentRect.s;
             var b = currentRect.b + hoverDeviation;
+            currentRect.env.play();
         } else {
-            var r = currentRect.r;
-            var g = currentRect.g;
+            var hue = currentRect.hue;
+            var s = currentRect.s;
             var b = currentRect.b;
         }
-        fill(r, g, b, 255);
-        rect(currentRect.x, currentRect.y, currentRect.w, currentRect.h, 20);
-    };
+        colorMode(HSB, 255);
+        fill(hue, s, b, 255);
+        rect(currentRect.x, currentRect.y, currentRect.w, currentRect.height, 20);
+    }
 }
 
 function mousePressed() {
